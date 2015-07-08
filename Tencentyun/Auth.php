e0adabf0c6c899473dfb46bf2332f4f637aabc1c
wangnan8791 <wangnan8791@gmail.com>
2015-07-08 15:25:37 +0800

    update Auth appSign to private, to avoid abuse

--- Tencentyun/Auth.php
+++ Tencentyun/Auth.php
@@ -7,32 +7,6 @@ class Auth
     const AUTH_URL_FORMAT_ERROR = -1;
     const AUTH_SECRET_ID_KEY_ERROR = -2;
 
-    /**
-     * 签名函数（上传、下载会生成多次有效签名，删除资源会生成单次有效签名）
-     * @param  string $appId
-     * @param  string $secretId
-     * @param  string $secretKey
-     * @param  int $expired       过期时间,unix时间戳
-	 * @param  string $fileId     文件路径，以 /{$appId}/{$bucketName} 开头
-	 * @param  string $bucketName 文件所在bucket
-     * @return string             签名
-     */
-    public static function appSign($appId, $secretId, $secretKey, $expired, $fileId, $bucketName) {
-		if (empty($secretId) || empty($secretKey)) {
-			return self::AUTH_SECRET_ID_KEY_ERROR;
-		}
-
-		$now = time();
-		$rdm = rand();
-		$plainText = "a=$appId&k=$secretId&e=$expired&t=$now&r=$rdm&f=$fileId&b=$bucketName";
-		$bin = hash_hmac('SHA1', $plainText, $secretKey, true);
-		$bin = $bin.$plainText;
-
-		$sign = base64_encode($bin);
-
-		return $sign;
-    }
-
 	 /**
      * 生成单次有效签名函数（用于删除和更新指定fileId资源，使用一次即失效）
 	 * @param  string $fileId     文件路径，以 /{$appId}/{$bucketName} 开头
@@ -69,6 +43,32 @@ class Auth
         }
 
         return self::appSign($appId, $secretId, $secretKey, $expired, null, $bucketName);
+    }
+
+    /**
+     * 签名函数（上传、下载会生成多次有效签名，删除资源会生成单次有效签名）
+     * @param  string $appId
+     * @param  string $secretId
+     * @param  string $secretKey
+     * @param  int $expired       过期时间,unix时间戳
+	 * @param  string $fileId     文件路径，以 /{$appId}/{$bucketName} 开头
+	 * @param  string $bucketName 文件所在bucket
+     * @return string             签名
+     */
+    private static function appSign($appId, $secretId, $secretKey, $expired, $fileId, $bucketName) {
+		if (empty($secretId) || empty($secretKey)) {
+			return self::AUTH_SECRET_ID_KEY_ERROR;
+		}
+
+		$now = time();
+		$rdm = rand();
+		$plainText = "a=$appId&k=$secretId&e=$expired&t=$now&r=$rdm&f=$fileId&b=$bucketName";
+		$bin = hash_hmac('SHA1', $plainText, $secretKey, true);
+		$bin = $bin.$plainText;
+
+		$sign = base64_encode($bin);
+
+		return $sign;
     }
 
 }
