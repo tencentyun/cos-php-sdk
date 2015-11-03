b7492c4a6c977359c484eebe459595f1241cd327
jayli <knuth.fan@gmail.com>
2015-11-03 18:25:57 +0800

    not send 100-continue

--- Qcloud_cos/Http.php
+++ Qcloud_cos/Http.php
@@ -35,6 +35,7 @@ class Http
         $header = isset($rq['header']) ? $rq['header'] : array();
         $header[] = 'Method:'.$method;
         $header[] = 'User-Agent:'.Conf::getUA();
+        $header[] = 'Expect:';
         isset($rq['host']) && $header[] = 'Host:'.$rq['host'];
         curl_setopt($curlHandle, CURLOPT_HTTPHEADER, $header);
         curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
